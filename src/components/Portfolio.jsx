ce0288da3968116dff1898609f1086d4e34c477c
ethan2000liu <ethan2000liu@gmail.com>
2025-02-19 16:02:54 -0800

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,68 +9,76 @@ import ReactLiveChat from "../assets/portfolio/LiveChat.jpg";
 import ClipVista from "../assets/portfolio/ClipVista.png";
 import SchoolDatabase from "../assets/portfolio/SchoolDatabase.jpg";
 import PokemonIndex from "../assets/portfolio/PokemonIndex.png";
+import Chonk from "../assets/portfolio/Chonk.png";
 
 
 const Portfolio = () => {
     const portfolios = [
         {
             id: 1,
+            src: Chonk,
+            description: "C.H.O.N.K (Convolutional Heftiness Observer for Notable Kitties) - A sophisticated deep learning project leveraging computer vision for cat classification. Engineered automated data collection using DDG API with 2400+ images, developed robust ETL pipeline, and implemented a continuous learning system with TensorFlow and ResNet50. Features include data validation, multi-stage checks, and user feedback integration for ongoing model improvement.",
+            link: "https://github.com/ethan2000liu/-C.H.O.N.K",
+            showButton: true
+        },
+        {
+            id: 2,
             src: AIStoryTeller,
             description: "Innovatively developed an AI Storyteller app, seamlessly integrating computer vision and NLP technologies. Demonstrated expertise by leveraging Hugging Face's Transformers for image-to-text and integrating GPT-2 for creative story generation. Proficient in Python, ML, and deep learning techniques, with additional capabilities showcased through OpenAI API for NLP tasks.",
             link: "https://github.com/ethan2000liu/AI-story-teller",
             showButton: true
         },
         {
-            id: 2,
+            id: 3,
             src: AIStoryPainter,
             description: "Innovatively developed an AI Storypainter app, seamlessly integrating computer vision and NLP technologies. Demonstrated expertise by leveraging Hugging Face's Transformers for image generation and integrating LLM. Comfortable with different models. Hosted and managed project on GitHub, highlighting version control practices. ",
             link: "https://github.com/ethan2000liu/AI-story-painter",
             showButton: true
         },
         {
-            id: 3,
+            id: 4,
             src: SearchandRescue,
             description: "Developed Raspberry Pi project to locate and rescue individuals in remote areas with low visibility. Developed a program in C to fetch real-time GPS coordinates and thermal camera images. Used Python for SMS communication and OpenCV and Tensorflow to assist with image processing. Developed embedded software that act as a hub for all of the components. ",
             link: "/search-and-rescue-details",
             showButton: false
         },
         {
-            id: 4,
+            id: 5,
             src: SchoolDatabase,
             description: "Developed database with MySql based on the information pro…Used React to develop a web app to fetch data from PokeAPI and display Pokémon details.  ",
             link: "/school-database-details",
             showButton: false
         },
         {
-            id: 5,
+            id: 6,
             src: ReactLiveChat,
             description: "Developed a real-time chat application using Node.js, React.js, and ChatEngine.io. Implemented user authentication, socket connections, and real-time messaging with features such as group chats, direct messages, and file attachments. Troubleshot and debugged to ensure seamless user experiences, showcasing proficiency in full-stack development and effective problem-solving. ",
             link: "https://github.com/ethan2000liu/FullStack-Live-Chat",
             showButton: true
         },
         {
-            id: 6,
+            id: 7,
             src: HomeworkHelper,
             description: "Developed a Discord bot using Python and Node.js to assist students with homework and tests. Discord server grew to over 200+ students using the service. ",
             link: "/homework-helper-details",
             showButton: false
         },
         {
-            id: 7,
+            id: 8,
             src: SoilMoistureizer,
             description: "Developed embedded software in C to interface with GPIO for capturing moisture levels in soil. Implemented an analog-to-digital converter to display values on an LCD screen and make decisions using a finite state machine. ",
             link: "/soil-moistureizer-details",
             showButton: false
         },
         {
-            id: 8,
+            id: 9,
             src: ClipVista,
             description: "YouTube-inspired Platform: Develop a video-sharing platform resembling YouTube. Key Features: Fetch video data (views, likes, channels), enable search functionality, categorize videos. User Experience: Focus on intuitive interface, efficient content delivery, and user engagement features. ",
             link: "https://clipvista.netlify.app",
             showButton: true
         },
         {
-            id: 9,
+            id: 10,
             src: PokemonIndex,
             description: "Used React to develop a web app to fetch data from PokeAPI and display Pokémon details. ",
             link: "https://github.com/kfc0105/pokedexApp",
